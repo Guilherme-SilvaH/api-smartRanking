922a7e91f3bd50812cb5f2c26f302af5a7d2e92b
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-05-02 19:51:20 -0300

    update-JogadoresController

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,19 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
-
+import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
 
+    constructor(private readonly jogadoresService: JogadoresService){}
+
     @Post()
+    //metodo
     async criarAtualizarJogador(
         //cria um body para extrair um obj da req com a estancia "criaJogadorDto" que é do tipo "CriarJogadorDto"
-        @Body() criaJogadorDto: CriarJogadorDto
-    ){
-        //handler
-        const {email} = criaJogadorDto
-        return JSON.stringify(`{
-            "email": ${email}
-        }`)
-    } 
+        @Body() criaJogadorDto: CriarJogadorDto){
+            await this.jogadoresService.criarAtualizarJogador(criaJogadorDto)
+        } 
 }   
