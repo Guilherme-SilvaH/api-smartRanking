0f64ec8cb38895139558b5ac091e369accd36ab6
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-05-13 16:36:05 -0300

    interface-categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:93929136gGui@cluster0.gj5iy6p.mongodb.net/smartranking?retryWrites=true&w=majority&appName=Cluster0'),
     
-    JogadoresModule],
+    JogadoresModule,
+    
+    CategoriasModule],
   controllers: [],
   providers: [],
 })
