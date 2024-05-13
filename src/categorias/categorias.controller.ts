7b566a861256b9ab6a79e9c8ac3f8d3b929afc9f
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-05-13 17:12:08 -0300

    Service-CriarCategoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { CategoriasService } from './categorias.service';
+import { Categoria } from './interface/categorias.interface';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+
+    constructor(private readonly categoriasService: CategoriasService){}
+
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarCategoria(
+        @Body() criarCategoriaDto: CriarCategoriaDto): Promise<Categoria>{
+            return await this.categoriasService.criarCategoria(criarCategoriaDto)
+        }
+}
---
+++ src/categorias/dtos/criar-categoria.dto.ts
@@ -0,0 +1,21 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from "class-validator";
+import { Evento } from "../interface/categorias.interface";
+
+
+export class CriarCategoriaDto {
+
+
+    @IsString()
+    @IsNotEmpty()
+    readonly categoria: string;
+
+
+    @IsString()
+    @IsNotEmpty()
+    descricao: string;
+
+
+    @IsArray()
+    @ArrayMinSize(1)
+    eventos: Array<Evento>
+}
