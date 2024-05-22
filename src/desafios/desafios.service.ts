93a087e9e98f2e8f6fea9ba4e49e7593ea3907f2
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-05-22 18:56:11 -0300

    verificar

--- src/desafios/desafios.service.ts
+++ src/desafios/desafios.service.ts
@@ -10,37 +10,25 @@ import { JogadoresService } from "src/jogadores/jogadores.service";
 @Injectable()
 export class desafiosService{
     constructor(
-        @InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>,
         @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
         private readonly jogadoresService: JogadoresService){}
     
     //metodo
-    async criarDesafio(criarDesafioDto: criarDesafioDto): Promise<Desafio> {
-        const { jogadores, solicitante } = criarDesafioDto;
+  async criarDesafio(criarDesafioDto: criarDesafioDto): Promise<Desafio> {
 
-        // Verificar se os jogadores informados estão cadastrados no banco de dados
-        const jogadoresIds = jogadores.map(jogador => jogador._id);
-        const jogadoresEncontrados = await Promise.all(
-            jogadoresIds.map(async id => {
-                try {
-                    return await this.jogadoresService.consultarJogadorPeloId(id);
-                } catch (error) {
-                    return null; // Se não encontrar, retorna null
-                }
-            })
-        );
 
-        if (jogadoresEncontrados.includes(null)) {
-            throw new NotFoundException(`Um ou mais jogadores não foram encontrados`);
+    const jogadores = await this.jogadoresService.consultarTodosJogadores()
+
+    criarDesafioDto.jogadores.map(jogadorDto => {
+        const jogadorFilter = jogadores.filter( jogador => jogador._id == jogadorDto._id )
+
+        if (jogadorFilter.length == 0) {
+            throw new BadRequestException(`O id ${jogadorDto._id} não é um jogador!`)
         }
+    
+    })
 
-        // Verificar se o solicitante é um dos jogadores do desafio
-        const solicitanteId = solicitante._id;
-        const isSolicitanteValid = jogadoresIds.includes(solicitanteId);
 
-        if (!isSolicitanteValid) {
-            throw new BadRequestException(`O solicitante deve ser um dos jogadores do desafio`);
-        }
 
         const desafioCriado = new this.desafioModel(criarDesafioDto);
         return await desafioCriado.save();
